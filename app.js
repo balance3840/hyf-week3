99ea23bc72ca8995fba6ba0de90d422ef50b241e
Ramiro Estrella <ramiro@hococo.io>
2023-12-10 10:52:35 +0100

    Inittial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// Define database in connection.js
+// Create contacts router
+
+
+// Import express
+// Initialize express
+// Import contacts router
---
+++ connection.js
@@ -0,0 +1,11 @@
+// Define knex database connection
+// {
+//     client: 'mysql',
+//     connection: {
+//         host: '127.0.0.1',
+//         port: 3306,
+//         user: 'USERNAME',
+//         password: 'PASSWORD',
+//         database: 'DATABASE_NAME'
+//     }
+// }
